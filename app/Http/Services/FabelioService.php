89ee1544a7c9abad1865770dc4ae964ead7b4c5f
dels07 <crushfire2004@gmail.com>
2019-12-11 11:08:34 +0700

    feat(product): add web scrapper to fabelio service

---
+++ app/Http/Services/FabelioService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Services;
+
+use Exception;
+use Goutte\Client;
+
+class FabelioService
+{
+    /**
+     * Get Fabelio product information.
+     *
+     * @param string $url
+     * @return array $json
+     */
+    public function getProductInfo(string $url)
+    {
+        if (!$url) {
+            throw new Exception('url cannot be empty!');
+        }
+
+        $client = new Client;
+        $crawler = $client->request('GET', $url);
+
+        // scrap page for informations
+        $productId = $crawler->filter('#productId')->attr('value');
+        $title = $crawler->filter('.base')->extract(['_text'])[0];
+        $altTitle = $crawler->filter('.page-title__secondary')->extract(['_text'])[0];
+        $description = $crawler->filter('#description')->html();
+        $additionalInfo = $crawler->filter('.product-info__attributes')->html();
+        $price = $crawler->filter("#product-price-{$productId} > span")->extract(['_text'])[0];
+
+        // post processing
+        $description = str_replace(["<br>"], '', $description);
+        $price = (int) str_replace(['Rp ', '.'], '', $price);
+
+        // scrap image
+        $images = file_get_contents("https://fabelio.com/swatches/ajax/media/?product_id={$productId}");
+
+        $json = [
+            'product_id' => $productId,
+            'title' => $title,
+            'alt_title' => $altTitle,
+            'description' => $description,
+            'additional_info' => $additionalInfo,
+            'price' => $price,
+            'url' => $url,
+            'images' => $images
+        ];
+
+        return $json;
+    }
